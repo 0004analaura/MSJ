e7307baabb6601d623eb67da6f90c0839471be26
Ana Laura Oliva <aolivac4@miumg.edu.gt>
2024-05-07 20:36:40 -0600

    primer comentario

---
+++ MSJ/MSJ/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MSJ
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hola nueva prueba");
+        }
+    }
+}
